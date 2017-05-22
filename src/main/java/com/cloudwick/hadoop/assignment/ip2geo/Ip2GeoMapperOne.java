6389f028862c571b7d4687f1ebc6f69eec75724a
nikhiljabade <nikhil.jabade@gmail.com>
2017-05-22 02:05:14 -0700

    Update Ip2GeoMapperOne.java

--- src/main/java/com/cloudwick/hadoop/assignment/ip2geo/Ip2GeoMapperOne.java
+++ src/main/java/com/cloudwick/hadoop/assignment/ip2geo/Ip2GeoMapperOne.java
@@ -14,8 +14,6 @@ public class Ip2GeoMapperOne extends Mapper<LongWritable, Text, Text, Text> {
         String[] columns = line.split(" ");
         setKey.set(columns[2]);
         setValue.set(columns[0] + " " + columns[1]);
-        System.out.println("Key 1111 > " + setKey);
-        System.out.println("Value 1111 > " + setValue);
         context.write(setKey, setValue);
     }
 }
